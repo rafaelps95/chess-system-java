b4f6742764188f4bb301aae7a25036160ce0d6de
Rafael Pedro da Silva <rafaelpedrodasilva@outlook.com>
2019-07-18 20:48:41 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChessException extends RuntimeException{
+
+	private static final long serialVersionUID = 6492993152489147354L;
+
+	public ChessException(String message) {
+		super(message);
+	}
+}
